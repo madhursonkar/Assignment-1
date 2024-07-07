c7912df8073ec131e9a992b0dcd110c01ec40208
Madhur Sonkar <138878209+madhursonkar@users.noreply.github.com>
2024-07-07 12:32:12 +0530

    Add files via upload

---
+++ Assignment 1/script.js
@@ -0,0 +1,177 @@
+// Code for question 1
+let correct_ans1 = document.querySelector("#q1b3") ;
+correct_ans1.onclick = () => {
+    let place = document.querySelector("#ansplace1") ;
+    place.innerHTML = "<p>Correct Answer !!</p>" ;
+    place.style.color = "green" ;
+    place.style.marginLeft = "20px" ;
+    correct_ans1.style.backgroundColor = "green" ;
+}
+
+let wrong_ans11 = document.querySelector("#q1b1") ;
+wrong_ans11.onclick = () => {
+    let place = document.querySelector("#ansplace1") ;
+    place.innerHTML = "<p>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let wrong_ans12 = document.querySelector("#q1b2") ;
+wrong_ans12.onclick = () => {
+    let place = document.querySelector("#ansplace1") ;
+    place.innerHTML = "<p>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let wrong_ans14 = document.querySelector("#q1b4") ;
+wrong_ans14.onclick = () => {
+    let place = document.querySelector("#ansplace1") ;
+    place.innerHTML = "<p>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+
+
+// Code for question 2
+let wrong_ans21 = document.querySelector("#q2b1") ;
+wrong_ans21.onclick = () => {
+    let place = document.querySelector("#ansplace2") ;
+    place.innerHTML = "<p>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let correct_ans2 = document.querySelector("#q2b2") ;
+correct_ans2.onclick = () => {
+    let place = document.querySelector("#ansplace2") ;
+    place.innerHTML = "<p>Correct Answer !!</p>" ;
+    place.style.color = "green" ;
+    place.style.marginLeft = "20px" ;
+    correct_ans2.style.backgroundColor = "green" ;
+}
+
+let wrong_ans23 = document.querySelector("#q2b3") ;
+wrong_ans23.onclick = () => {
+    let place = document.querySelector("#ansplace2") ;
+    place.innerHTML = "<p>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let wrong_ans24 = document.querySelector("#q2b4") ;
+wrong_ans24.onclick = () => {
+    let place = document.querySelector("#ansplace2") ;
+    place.innerHTML = "<p>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+
+
+// Code for question 3
+let wrong_ans31 = document.querySelector("#q3b1") ;
+wrong_ans31.onclick = () => {
+    let place = document.querySelector("#ansplace3") ;
+    place.innerHTML = "<p style>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let wrong_ans32 = document.querySelector("#q3b2") ;
+wrong_ans32.onclick = () => {
+    let place = document.querySelector("#ansplace3") ;
+    place.innerHTML = "<p style>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let correct_ans3= document.querySelector("#q3b3") ;
+correct_ans3.onclick = () => {
+    let place = document.querySelector("#ansplace3") ;
+    place.innerHTML = "<p style>Correct Answer !!</p>" ;
+    place.style.color = "green" ;
+    place.style.marginLeft = "20px" ;
+    correct_ans3.style.backgroundColor = "green" ;
+}
+
+let wrong_ans34 = document.querySelector("#q3b4") ;
+wrong_ans34.onclick = () => {
+    let place = document.querySelector("#ansplace3") ;
+    place.innerHTML = "<p style>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+
+
+// Code for question 4
+let wrong_ans41 = document.querySelector("#q4b1") ;
+wrong_ans41.onclick = () => {
+    let place = document.querySelector("#ansplace4") ;
+    place.innerHTML = "<p style>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let wrong_ans42 = document.querySelector("#q4b2") ;
+wrong_ans42.onclick = () => {
+    let place = document.querySelector("#ansplace4") ;
+    place.innerHTML = "<p style>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let correct_ans4 = document.querySelector("#q4b3") ;
+correct_ans4.onclick = () => {
+    let place = document.querySelector("#ansplace4") ;
+    place.innerHTML = "<p style>Correct Answer !!</p>" ;
+    place.style.color = "green" ;
+    place.style.marginLeft = "20px" ;
+    correct_ans4.style.backgroundColor = "green" ;
+}
+
+let wrong_ans44 = document.querySelector("#q4b4") ;
+wrong_ans44.onclick = () => {
+    let place = document.querySelector("#ansplace4") ;
+    place.innerHTML = "<p style>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+
+
+// Code for question-5
+let wrong_ans51 = document.querySelector("#q5b1") ;
+wrong_ans51.onclick = () => {
+    let place = document.querySelector("#ansplace5") ;
+    place.innerHTML = "<p style>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let wrong_ans52 = document.querySelector("#q5b2") ;
+wrong_ans52.onclick = () => {
+    let place = document.querySelector("#ansplace5") ;
+    place.innerHTML = "<p style>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let wrong_ans53 = document.querySelector("#q5b3") ;
+wrong_ans53.onclick = () => {
+    let place = document.querySelector("#ansplace5") ;
+    place.innerHTML = "<p style>Wrong Answer !!</p>" ;
+    place.style.color = "red" ;
+    place.style.marginLeft = "20px" ;
+}
+
+let correct_ans5 = document.querySelector("#q5b4") ;
+correct_ans5.onclick = () => {
+    let place = document.querySelector("#ansplace5") ;
+    place.innerHTML = "<p style>Correct Answer !!</p>" ;
+    place.style.color = "green" ;
+    place.style.marginLeft = "20px" ;
+    correct_ans5.style.backgroundColor = "green" ;
+}
